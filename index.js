29ab61c946dfe04635bb0818826e028f85582cff
Zack Argyle <zackargyle@pinterest.com>
2016-06-04 19:52:34 -0700

    Update README.md, demo, and screenshot

--- index.js
+++ index.js
@@ -18,20 +18,20 @@ const history = [
 ];
 
 const structure = {
-    '.private': {
-        file1: { content: 'File1 in private' },
-        file2: { content: 'File2 in private' },
+    '.hidden': {
+        file1: { content: 'The is the content for file1 in the <.hidden> directory.' },
+        file2: { content: 'The is the content for file2 in the <.hidden> directory.' },
         dir2: {
-            file: { content: 'File in private/dir2' },
+            file: { content: 'The is the content for <file> in the <.hidden> directory.' },
         },
         '.secrets': { content: 'I\'m still afraid of the dark...' },
     },
     public: {
-        file1: { content: 'File2 in public' },
-        file2: { content: 'File2 in public' },
-        file3: { content: 'File2 in public' },
+        file1: { content: 'The is the content for file1 in the <public> directory.' },
+        file2: { content: 'The is the content for file2 in the <public> directory.' },
+        file3: { content: 'The is the content for file3 in the <public> directory.' },
     },
-    README: { content: 'This is a demo README. ✌⊂(✰‿✰)つ✌' },
+    'README.md': { content: '✌⊂(✰‿✰)つ✌ Thanks for checking out the tool! There is a lot that you can do with react-bash and I\'m excited to see all of the fun commands and projects build on top of it!' },
 };
 
 const Root = <ReactBash history={history} structure={structure} extensions={extensions} />;
