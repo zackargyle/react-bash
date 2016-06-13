882a37aa7bb64fef24f000369a75461970f06ccb
Zack Argyle <zackargyle@pinterest.com>
2016-06-13 12:05:24 -0700

    Fix build to exclude React dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,46 @@
-'use strict';
+var webpack = require('webpack');
 
-var webpack = require('webpack')
+var reactExternal = {
+    root: 'React',
+    commonjs2: 'react',
+    commonjs: 'react',
+    amd: 'react'
+};
 
-var env = process.env.NODE_ENV
+var env = process.env.NODE_ENV;
 var config = {
-  module: {
-    loaders: [
-      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
+    externals: {
+        react: reactExternal
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
+        ]
+    },
+    output: {
+        library: 'ReactBash',
+        libraryTarget: 'umd'
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(env)
+        })
     ]
-  },
-  output: {
-    library: 'ReactBash',
-    libraryTarget: 'umd'
-  },
-  plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(env)
-    })
-  ]
 };
 
 if (env === 'production') {
-  config.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        warnings: false
-      }
-    })
-  )
+    config.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                pure_getters: true,
+                unsafe: true,
+                unsafe_comps: true,
+                screw_ie8: true,
+                warnings: false
+            }
+        })
+    )
 }
 
-module.exports = config
+module.exports = config;
